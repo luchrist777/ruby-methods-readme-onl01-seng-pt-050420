63db5c4e23dee92ca79550e1c1311328418f288b
Luis Leandro <luchrist777@gmail.com>
2020-05-09 04:29:32 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,16 @@
-def say_hello_ten_times
+def 
+say_hello_ten_times
+ phrase = "Hello World!"
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+end
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+say_hello_ten_times
