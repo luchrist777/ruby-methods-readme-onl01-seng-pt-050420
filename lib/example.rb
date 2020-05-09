cf380f9421d2e3d895e9e7553189868d796e5806
Luis Leandro <luchrist777@gmail.com>
2020-05-09 04:26:57 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,15 +1,16 @@
-def say_hello_ten_times
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def 
+say_hello_ten_times
+ phrase = "Hello World!"
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
 end
 
 say_hello_ten_times
